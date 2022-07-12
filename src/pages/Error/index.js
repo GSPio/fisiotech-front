7f5d4d89785283bb43e5fc1482d83249a42d8ce3
Guilherme Soares Pio <gpio@verg1759.lobtec.com.br>
2022-07-12 08:59:42 -0300

    v. 1.0.0

---
+++ src/pages/Error/index.js
@@ -0,0 +1,11 @@
+
+
+function Error () {
+    return (
+        <>
+            <h1>Não conseguimos encontrar a página que você está procurando...</h1>
+        </>
+    )
+}
+
+export default Error
---
+++ src/pages/Home/index.js
@@ -0,0 +1,14 @@
+import './index.css'
+
+function Home () {
+    return (
+        <div className='Home'>
+            <div>
+                <ul>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+export default Home
